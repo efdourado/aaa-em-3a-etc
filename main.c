eeec87783f1c10ae9b632c07ab5274b1cf80686c
Eduardo Dourado <146302470+efdourado@users.noreply.github.com>
2024-01-15 10:41:46 -0300

    aaa em 3a etc

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int contador = 1; char a [1001];
+
+    printf ("escreva uma frase (aaa será transformado em 3a etc): "); fgets (a, sizeof (a), stdin);
+
+    int b = strlen (a);
+
+    for (int i = 1; i <= b; i ++) {
+        if (a [i] == a [i-1]) contador ++;
+        else {
+            if (contador != 1) {
+                printf ("%d%c", contador, a [i -1]);
+            }
+            else {
+                printf ("%c", a [i -1]);
+            }
+            contador = 1;
+        }
+    }   
+    return 0;
+}
